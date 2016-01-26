62ea0f3228c2eb2907218b90b00ebc6dd5aac392
vrunoa <vruno.alassia@gmail.com>
2016-01-26 10:02:55 -0800

    check property type, Parse manages JSONObject as HashMap and JSONArray as List

--- serializer/src/main/java/com/urucas/parseobjectserializer/Serializer.java
+++ serializer/src/main/java/com/urucas/parseobjectserializer/Serializer.java
@@ -16,7 +16,8 @@ import org.json.JSONObject;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Objects;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by vruno on 1/25/16.
@@ -28,46 +29,53 @@ public abstract class Serializer {
         JSONObject json = new JSONObject();
         if(object == null) return json;
 
-        try {
-            String property = object.getObjectId();
-            json.put("id", property);
+        try { json.put("id", object.getObjectId());
+        }catch(Exception e){}
 
+        try {
             Date date = object.getCreatedAt();
-            json.put("createdAt", Serializer.ParseDate((Date)date));
+            json.put("createdAt", Serializer.ParseDate((Date) date));
 
             date = object.getUpdatedAt();
-            json.put("updatedAt", Serializer.ParseDate((Date)date));
+            json.put("updatedAt", Serializer.ParseDate((Date) date));
 
         }catch(Exception e){}
 
         for(String key: object.keySet()) {
-            try { Push(json, key, object.get(key)); }catch(Exception e){ continue; }
+            try {
+                Object property = object.get(key);
+                if(property instanceof HashMap)
+                    property = object.getJSONObject(key);
+
+                if(property instanceof List)
+                    property = object.getJSONArray(key);
+
+                Push(json, key, property);
+
+            }catch(Exception e){
+                e.printStackTrace();
+                continue;
+            }
         }
         return json;
     }
 
-    public static JSONObject Push(JSONObject json, String key, Object property) {
-        try {
-            if (property == null)
-                return json.put(key, "");
+    public static JSONObject Push(JSONObject json, String key, Object property) throws JSONException, ParseException {
 
-            if (property instanceof String || property instanceof Boolean
+        if (property == null) return json.put(key, "");
+
+        if (property instanceof String || property instanceof Boolean
                     || property instanceof JSONArray || property instanceof JSONObject)
-                return json.put(key, property);
+            return json.put(key, property);
 
-            if (property instanceof Number)
-                return json.put(key, ((Number) property).doubleValue());
+        if (property instanceof Number) return json.put(key, ((Number) property).doubleValue());
 
-            if (property instanceof ParseFile)
-                return json.put(key, Serializer.Serialize((ParseFile) property));
+        if (property instanceof ParseFile) return json.put(key, Serializer.Serialize((ParseFile) property));
 
-            if (property instanceof ParseGeoPoint)
-                return json.put(key, Serializer.Serialize((ParseGeoPoint) property));
+        if (property instanceof ParseGeoPoint) return json.put(key, Serializer.Serialize((ParseGeoPoint) property));
 
-            if (property instanceof Date)
-                return json.put(key, Serializer.ParseDate((Date) property));
+        if (property instanceof Date) return json.put(key, Serializer.ParseDate((Date) property));
 
-        }catch(Exception e){}
         return json;
     }
 
