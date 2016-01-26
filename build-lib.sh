1a4428317630e8f815d0212b4b65ee244e4bec2d
vrunoa <vruno.alassia@gmail.com>
2016-01-26 10:16:54 -0800

    add build scripts

---
+++ build-lib.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+VERSION=$(./gradlew -q Version)
+echo "Project Version: ${VERSION}"
+RELEASE_NAME=$"./parseobjectserializer-${VERSION}.aar"
+if [ -f $RELEASE_NAME ]; then
+  rm $RELEASE_NAME 
+fi
+MAVEN_FILE="maven-metadata.xml"
+if [ -f $MAVEN_FILE ]; then
+  rm "maven-metadata.xml"
+fi
+if [ -d "./build" ]; then
+  rm -rf ./build
+fi
+echo "building release"
+./gradlew assembleRelease
+cp ./logcatio/build/outputs/aar/serializer-release.aar $RELEASE_NAME
+echo "generating pom"
+./gradlew createPom
+exit 0
---
+++ release-lib.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+VERSION=$(./gradlew -q Version)
+echo "Project version: ${VERSION}"
+RELEASE_NAME=$"./serializer-${VERSION}.aar"
+if [ ! -f $RELEASE_NAME ]; then
+  echo "buiild release $RELEASE_NAME not found!"
+  exit 1
+fi
+MAVEN_FILE="maven-metadata.xml"
+if [ ! -f $MAVEN_FILE ]; then
+  echo "buiild maven $MAVEN_FILE not found!"
+  exit 1
+fi
+git checkout gh-pages
+RELEASE_FOLDER=$"com/urucas/parseobjectserializer/${VERSION}/"
+if [ ! -d $RELEASE_FOLDER ]; then
+  mkdir -p $RELEASE_FOLDER
+fi
+RELEASE_PATH=$"${RELEASE_FOLDER}parseobjectserializer-${VERSION}.aar"
+mv $RELEASE_NAME $RELEASE_PATH
+MAVEN_PATH=$"com/urucas/parseobjectserializer/$MAVEN_FILE"
+mv $MAVEN_FILE $MAVEN_PATH
+git add $RELEASE_PATH
+git add $MAVEN_PATH
+git ci $RELEASE_PATH $MAVEN_PATH -m "update release ${VERSION}"
+git push origin gh-pages
+git checkout master
